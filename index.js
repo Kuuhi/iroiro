1c552725ef25a14a6221633a75881b1508f9c743
Kuuhi <kuuhi0pgmail.com>
2025-06-06 09:16:45 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildBans,
+		GatewayIntentBits.GuildEmojisAndStickers,
+		GatewayIntentBits.GuildIntegrations,
+		GatewayIntentBits.GuildWebhooks,
+		GatewayIntentBits.GuildInvites,
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.GuildPresences,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessageTyping,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.DirectMessageReactions,
+		GatewayIntentBits.DirectMessageTyping,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildScheduledEvents,
+	],
+	partials: [
+		Partials.User,
+		Partials.Channel,
+		Partials.GuildMember,
+		Partials.Message,
+		Partials.Reaction,
+		Partials.GuildScheduledEvent,
+		Partials.ThreadMember,
+	],
+});
+
+client.once('ready', () => {
+	console.log('起動完了');
+});
+
+require('dotenv').config()
+
+client.login(process.env.BOT_TOKEN);
+
+client.on("messageCreate", async (message) => {
+  if (message.content==="e+hi") {
+        message.reply({
+            content: "hi!",
+            allowedMentions: { parse: []}
+        })
+      }
+}) 
