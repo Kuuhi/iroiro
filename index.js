480b84e5f158c7665470158c24e95c3ccf429665
Kuuhi <kuuhi0pgmail.com>
2025-06-09 11:09:11 +0900

    update index

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.GuildBans,
-		GatewayIntentBits.GuildEmojisAndStickers,
-		GatewayIntentBits.GuildIntegrations,
-		GatewayIntentBits.GuildWebhooks,
-		GatewayIntentBits.GuildInvites,
-		GatewayIntentBits.GuildVoiceStates,
-		GatewayIntentBits.GuildPresences,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.GuildMessageReactions,
-		GatewayIntentBits.GuildMessageTyping,
-		GatewayIntentBits.DirectMessages,
-		GatewayIntentBits.DirectMessageReactions,
-		GatewayIntentBits.DirectMessageTyping,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildScheduledEvents,
-	],
-	partials: [
-		Partials.User,
-		Partials.Channel,
-		Partials.GuildMember,
-		Partials.Message,
-		Partials.Reaction,
-		Partials.GuildScheduledEvent,
-		Partials.ThreadMember,
-	],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildScheduledEvents,
+  ],
+  partials: [
+    Partials.User,
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.GuildScheduledEvent,
+    Partials.ThreadMember,
+  ],
 });
 
 client.once('ready', () => {
-	console.log('起動完了');
+  console.log('起動完了');
 });
 
 require('dotenv').config()
@@ -40,10 +40,10 @@ require('dotenv').config()
 client.login(process.env.BOT_TOKEN);
 
 client.on("messageCreate", async (message) => {
-  if (message.content==="e+hi") {
-        message.reply({
-            content: "hi!",
-            allowedMentions: { parse: []}
-        })
-      }
+  if (message.content === "e+hi") {
+    message.reply({
+      content: "hi!",
+      allowedMentions: { parse: [] }
+    })
+  }
 }) 
