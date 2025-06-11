04363e8706ffa3b1e01d40c8f435b983b0a301af
Kuuhi <kuuhi0pgmail.com>
2025-06-11 12:44:57 +0900

    add commands/reboot

---
+++ commands/reboot.js
@@ -0,0 +1,14 @@
+module.exports = {
+  data: {
+    name: 'reboot',
+    description: '(管理者)ボットを再起動します（PM2が自動的に起動します）。',
+  },
+
+  adminIds: ["777466773955936266"],
+
+  async execute(message) {
+    if (!this.adminIds.includes(message.author.id)) return
+    await message.react("✅")
+    process.exit(0);
+  },
+};
